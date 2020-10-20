ef6ce979159c29ecc1ba6b396aaa95febcd4c443
sud7535 <sud7535@gmail.com>
2020-10-20 13:40:10 +0530

    Initializing Models
    	new file:   PokeTCG/__pycache__/__init__.cpython-38.pyc
    	new file:   PokeTCG/__pycache__/settings.cpython-38.pyc
    	new file:   PokeTCG/__pycache__/urls.cpython-38.pyc
    	modified:   PokeTCG/settings.py
    	new file:   backend/__pycache__/__init__.cpython-38.pyc
    	new file:   backend/__pycache__/admin.cpython-38.pyc
    	new file:   backend/__pycache__/apps.cpython-38.pyc
    	new file:   backend/__pycache__/models.cpython-38.pyc
    	new file:   backend/migrations/0001_initial.py
    	new file:   backend/migrations/__pycache__/0001_initial.cpython-38.pyc
    	new file:   backend/migrations/__pycache__/__init__.cpython-38.pyc
    	modified:   backend/models.py
    	new file:   db.sqlite3

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    identity = models.IntegerField(primary_key=True)
+    username = models.TextField(max_length=100)
+    password = models.TextField()
+
+class Deck(models.Model):
+    identity = models.IntegerField()
+    rank = models.IntegerField()
+    amount = models.IntegerField()

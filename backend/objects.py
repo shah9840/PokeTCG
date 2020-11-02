351a8c24932ffe2b87a7a07dca7951ef2ae5e0f9
sud7535 <sud7535@gmail.com>
2020-11-02 19:30:33 +0530

    Added maincard html and javascript
    	modified:   PokeTCG/settings.py
    	modified:   backend/objects.py
    	modified:   backend/static/style.css
    	modified:   backend/templates/backend/index.html
    	modified:   backend/views.py

--- backend/objects.py
+++ backend/objects.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 import sqlite3
 import os.path
-
+import json
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "poke.db")
 
+import random
+def randno():
+    r=[]
+    while(len(r)<=20):
+        z=random.randrange(1,252)
+        if(z not in r): # and z not in a and z not in b):
+            r.append(z)
+    return r
 
 def getPoke(num):
     with sqlite3.connect('backend/poke.db') as conn:
         c = conn.cursor()
         c.execute('''SELECT * FROM pokedex where Rank = ?''',[num])
-        obj =list(dict.fromkeys(c.fetchall()))
+        obj = list(dict.fromkeys(c.fetchall()))
     return obj[0]
 
 
@@ -28,13 +36,18 @@ class Pokedex():
         self.sdefense = obj[8]
         self.speed = obj[9]
         self.url = '/static/img/'+obj[0]+'.png'
+    def to_list(self):
+        lists =[self.rank,self.name,self.height,self.weight,self.hp,self.attack,self.defense,
+                self.sattack,self.sdefense,self.speed,self.url]
+        return lists
+
 
 def getobjects():
-    pokearray= []
-    for i in range(1,20):
+    pokearray_json= []
+    pokearray = []
+    # x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    for i in randno():
         tempobj = Pokedex(i)
         pokearray.append(tempobj)
-    print(pokearray[0].url)
-    return pokearray
-
-getobjects()
+        pokearray_json.append(tempobj.to_list())
+    return pokearray,pokearray_json
--- backend/views.py
+++ backend/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from . import objects
+import json
 # Create your views here.
 
 def homepage(request):
-    pokelist=objects.getobjects()
+    pokelist,pokelist_json=objects.getobjects()
     return render(request,
-                  'backend/index.html', {'list':pokelist})
+                  'backend/index.html', {'list_json':json.dumps(pokelist_json),
+                                         'list':pokelist})
 
 def login(request):
     return render(request = request,

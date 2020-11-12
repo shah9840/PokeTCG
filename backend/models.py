b04f0656bba2ab420f1258f2bf05fa5e71a0c581
sud7535 <sud7535@gmail.com>
2020-11-13 01:01:44 +0530

    Added sb.sqlite to gitignore
    	modified:   .gitignore
    	modified:   backend/models.py
    	modified:   backend/templates/backend/index.html

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Account(models.Model):
-    identity = models.IntegerField(primary_key=True)
-    username = models.TextField(max_length=100)
-    password = models.TextField()
-
-class Deck(models.Model):
-    identity = models.IntegerField()
-    rank = models.IntegerField()
-    amount = models.IntegerField()
